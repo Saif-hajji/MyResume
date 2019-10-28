671d69af7708e23c766e5f16da3b630d43c410a5
saif-hajji <saifedine.hajji@gmail.com>
2019-10-28 23:23:57 +0100

    Updates

---
+++ precache-manifest.651c9a5a9e2ba5909136a45bd8d21912.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c44190fc89f8756f9263c078a293b37",
+    "url": "/MyResume/index.html"
+  },
+  {
+    "revision": "2ace3ef3723107f30b55",
+    "url": "/MyResume/static/css/2.6493db97.chunk.css"
+  },
+  {
+    "revision": "ef6fe4324dabebe58296",
+    "url": "/MyResume/static/css/main.3e505a12.chunk.css"
+  },
+  {
+    "revision": "2ace3ef3723107f30b55",
+    "url": "/MyResume/static/js/2.5b1c6b4f.chunk.js"
+  },
+  {
+    "revision": "ef6fe4324dabebe58296",
+    "url": "/MyResume/static/js/main.2f192c46.chunk.js"
+  },
+  {
+    "revision": "5c76032a34d7820ad7cd",
+    "url": "/MyResume/static/js/runtime-main.29d7b9d1.js"
+  },
+  {
+    "revision": "d3d553d16cff73a22a4e6365cb609177",
+    "url": "/MyResume/static/media/IMG_1014.d3d553d1.JPG"
+  }
+]);
